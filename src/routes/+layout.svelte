6925beddae10db66758d7e372f35f636e0926ac7
Jonathan Brown <jbrown@acuity.network>
2026-05-04 17:51:50 +0700

    Add custom Kusama-based favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import { loadInjectedAccounts } from '$lib/services/accounts.svelte';
 	import { startAccountBalanceWatcher } from '$lib/services/balances.svelte';
 	import { startAppConnections } from '$lib/services/connections.svelte';
@@ -21,5 +20,4 @@
 	});
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
 {@render children()}
